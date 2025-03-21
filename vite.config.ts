d21f7c9042a7549642168958182190cdf7911d83
LiuRJ99 <17602192291@163.com>
2025-03-22 00:43:34 +0800

    增加了本地变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
   plugins: [
     vue(),
     vueJsx(),
@@ -22,11 +26,11 @@ export default defineConfig({
     proxy: {
       // 将JIRA API请求代理到JIRA服务器
       '/jira-api': {
-        target: 'http://xx.xx.xx.xx:8080',
+        target: env.VITE_JIRA_SERVER,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/jira-api/, ''),
         secure: false
       }
     }
   }
-})
+}})
